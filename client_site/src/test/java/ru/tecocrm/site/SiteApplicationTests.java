85129290b1f76d8f5dfbaf817f0392f0e4d2289e
kanayaya <kanayaya@mail.ru>
2024-11-06 15:17:09 +0800

    add client_site subproject

---
+++ client_site/src/test/java/ru/tecocrm/site/SiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tecocrm.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
